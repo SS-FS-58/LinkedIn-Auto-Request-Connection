78b6857165edf7070aed83aa248ffd37c2b40389
SS-FS-58 <ziqiangj6@gmail.com>
2022-04-18 16:37:25 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LinkedIn-Auto-Request-Connection
 This repository would implement the function to send a connection request with a note on special search url.
+
+please set the parameters DEFAULT_PROFILE, SEARCH_LINK, SEND_MESSAGE on .env file.
--- linkedin_auto_connection.py
+++ linkedin_auto_connection.py
@@ -1,14 +1,7 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import json
-from datetime import datetime
 from selenium.webdriver.common.action_chains import ActionChains
-import csv
 from threading import Thread, Event
 from time import sleep
 
