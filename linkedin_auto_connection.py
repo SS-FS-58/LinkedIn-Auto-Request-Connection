23891a5e9c35512cb062ca8d54b4adcf50eabb38
SS-FS-58 <ziqiangj6@gmail.com>
2022-04-18 16:35:54 +0000

    updated all code

--- linkedin_auto_connection.py
+++ linkedin_auto_connection.py
@@ -14,45 +14,87 @@ from time import sleep
 
 import os
 from dotenv import load_dotenv
-load_dotenv()
-DEFAULT_PROFILE = os.getenv('DEFAULT_PROFILE')
-SEARCH_LINK = os.getenv('SEARCH_LINK')
+
 
 class Bot(Thread):
     def __init__(self):
         Thread.__init__(self)
         self._stop = Event()
         self.total_request = 100
+        self.today_count = 0
+        self.page = 1
         self.setdriver()
+    
     def stop(self): 
         self._stop.set() 
+    
     def stopped(self): 
-        return self._stop.isSet() 
+        return self._stop.isSet()
+
+    def init_task_info(self):
+        load_dotenv()
+        self.DEFAULT_PROFILE = os.getenv('DEFAULT_PROFILE')
+        self.SEARCH_LINK = os.getenv('SEARCH_LINK')
+        self.SEND_MESSAGE = os.getenv('SEND_MESSAGE').replace('\\n','\n')
+
     def setdriver(self):
         try:
-            print('setting chrome driver')
+            self.init_task_info()
             options = webdriver.FirefoxOptions()
             options.add_argument("--start-maximized")
             # options.headless = True
-            profile = webdriver.FirefoxProfile(DEFAULT_PROFILE)
+            profile = webdriver.FirefoxProfile(self.DEFAULT_PROFILE)
             self.driver = webdriver.Firefox(executable_path="geckodriver.exe", options=options, firefox_profile=profile)
-            self.driver.get(SEARCH_LINK)
+            self.driver.get(self.SEARCH_LINK)
         except Exception as e:
             print(e)
-    def _check_connectable_button(self):
-        connectable_buttons = self.driver.find_elements(By.XPATH, '//button/span[text()="Connect"]')
-        for connectable_button in connectable_buttons:
-            connectable_button.click()
-            sleep(0.5)
-            add_note_button = self.driver.find_element(By.XPATH, '//button/span[text()="Add a note"]')
-            add_note_button.click()
-        print("found connectable button")
+    
+    def close(self):
+        try:
+            self.driver.quit()
+        except Exception as e:
+            print(e)
+
+    def _next_page(self):
+        self.page += 1
+        self.driver.get(f'{self.SEARCH_LINK}&page={self.page}')
+        print('next page : ', self.page)
+
+    def _click_button_with_label(self, label):
+        try:
+            self.driver.find_element(By.XPATH, f'//button/span[text()="{label}"]').click()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def perform_task(self):
+        try:
+            while self.today_count < self.total_request:
+                connectable_buttons = self.driver.find_elements(By.XPATH, '//button/span[text()="Connect"]')
+                for connectable_button in connectable_buttons:
+                    connectable_button.click()
+                    sleep(0.5)
+                    self.driver.find_element(By.XPATH, '//button/span[text()="Add a note"]').click()
+                    self._click_button_with_label("Add a note")
+                    self.driver.find_element(By.XPATH, '//textarea[@id="custom-message"]').send_keys(self.SEND_MESSAGE)
+                    if self._click_button_with_label("Send"):
+                        self.today_count += 1
+                    else:
+                        self._click_button_with_label("Cancel")
+                        actions = ActionChains(self.driver)
+                        actions.send_keys(Keys.ESCAPE)
+                        actions.perform()
+                self._next_page()
+        except Exception as e:
+            print(e)
+
     def run(self):
-        print('Checking page')
-        self._check_connectable_button()
-
-        
-
+        while True:
+            self.perform_task()
+            self.close()
+            sleep(3600*24)
+            self.setdriver()
 
 def main():
     my_bot = Bot()
